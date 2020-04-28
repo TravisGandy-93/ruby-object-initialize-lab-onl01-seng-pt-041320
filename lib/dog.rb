a215abe8865211e3fd64d8b1c36ebf30b6611c88
Travis Gandy <travis_gandy@yahoo.com>
2020-04-28 20:18:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
 class Dog 
   def initialize(name, breed)
     @name = name
-   if breed = "" 
-     @breed = "Mutt"
-     else 
+
        @breed = breed 
-      end 
-  
+      
   end 
   
   def name=(name)
